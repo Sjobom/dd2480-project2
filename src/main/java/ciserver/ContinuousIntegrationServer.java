39875610fc65808a39a32453cabe0e6499932061
Albin Remnestål <albin.remnestal@gmail.com>
2018-01-31 23:25:06 +0100

    feat: add outline for server middleware mechanism
    closes #20

--- src/main/java/ciserver/ContinuousIntegrationServer.java
+++ src/main/java/ciserver/ContinuousIntegrationServer.java
@@ -5,12 +5,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
  
 import java.io.IOException;
- 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-/** 
+/**
  Skeleton of a ContinuousIntegrationServer which acts as webhook
  See the Jetty documentation for API documentation of those classes.
 */
@@ -19,24 +21,50 @@ public class ContinuousIntegrationServer extends AbstractHandler
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
-                       HttpServletResponse response) 
+                       HttpServletResponse response)
         throws IOException, ServletException
     {
+        // Use regex to create a 'middleware' mechanism for routing within the server
+        Pattern r = Pattern.compile("^(\\/\\w*)(\\/.*)?");
+        Matcher m = r.matcher(target);
+        String route = m.find() ? m.group(1) : null;
+
+        // choose action with regard to target route
+        switch(route) {
+            case "/webhook":
+                // perform continous integration tasks
+                this.setResponse200(response, "webhook API");
+                break;
+            case "/build":
+                // fetch information about the requested build
+                this.setResponse200(response, "build API");
+                break;
+            default:
+                this.setResponse404(response);
+        }
+        baseRequest.setHandled(true);
+    }
+
+    /**
+     * Set the response status to 200 OK and add any additional payload
+     */
+    void setResponse200(HttpServletResponse response, String payload) throws IOException {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
-
-        System.out.println(target);
-
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
-
-        response.getWriter().println("CI job done");
+        if (payload != null) {
+            response.getWriter().println(payload);
+        }
     }
- 
-    // used to start the CI server in command line
+
+    /**
+     * set the response status to 404 Not Found
+     */
+    void setResponse404(HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    // start the CI server in command line
     public static void main(String[] args) throws Exception
     {
         Server server = new Server(8080);
