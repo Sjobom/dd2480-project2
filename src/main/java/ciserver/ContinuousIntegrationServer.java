59b2b791d5569c015037fb7afdcc67a153793507
Anders Sjöbom <anders.sjobom@live.se>
2018-02-02 13:22:54 +0100

    reafactor: changed methods to static in ContinousIntegrationServer
    Regarding #45

--- src/main/java/ciserver/ContinuousIntegrationServer.java
+++ src/main/java/ciserver/ContinuousIntegrationServer.java
@@ -60,31 +60,31 @@ public class ContinuousIntegrationServer extends AbstractHandler
      */
     void tryIntegration() {
         // 1st clone the repository
-        this.cloneRepository();
+        cloneRepository();
         // 2nd compile the code
-        this.compileCode();
+        compileCode();
         // 3rd build the code
-        this.runTests();
+        runTests();
     }
 
     /**
      * Clone the repository into temporary storage
      */
-    void cloneRepository() {
+    public static void cloneRepository() {
 
     }
 
     /**
      * Run the compile-procedure for the repository
      */
-    void compileCode() {
+    public static void compileCode() {
 
     }
 
     /**
      * Run the test-suite for the repository
      */
-    void runTests() {
+    public static void runTests() {
 
     }
 
