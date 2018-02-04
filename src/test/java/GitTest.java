37304dafa0a73f11d5d5424ca28c7bf5677fe901
Anders Sjöbom <anders.sjobom@live.se>
2018-02-04 19:08:38 +0100

    test: move dcreation of json test data for git cloning test to separate method
    Closes #52

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -13,23 +13,7 @@ public class GitTest {
         // Contract: Test if git cloning works
 
         // Create test data
-        JSONObject jsonObject = new JSONObject();
-        try {
-            BufferedReader webhook_file = new BufferedReader(new FileReader(System.getProperty("user.dir") +
-                    "//testData//git_webhook_baxterthehacker.json" ));
-            String line;
-            StringBuilder jsonData = new StringBuilder();
-            while ((line = webhook_file.readLine()) != null){
-                jsonData.append(line);
-            }
-            System.out.println(jsonData);
-            jsonObject = new JSONObject(jsonData.toString());
-        } catch (FileNotFoundException e) {
-            System.out.println("Could not find test data file!");
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        JSONObject jsonObject = getJsonTestData("git_webhook_baxterthehacker.json");
 
         // Test to clone the repository
         ContinuousIntegrationServer.cloneRepository(jsonObject);
@@ -44,6 +28,30 @@ public class GitTest {
         if(!FileSystemUtils.deleteRecursively(dir)) {
             System.out.println("Problem occurred when deleting the directory");
         }
+    }
+
+    /**
+     *  Helper method to create a json object from test data available
+     */
+    JSONObject getJsonTestData(String fileName) {
+        StringBuilder jsonData = new StringBuilder();
+        try {
+            BufferedReader webhook_file = new BufferedReader(new FileReader(System.getProperty("user.dir") +
+                    "//testData//" + fileName ));
+            String line;
+            while ((line = webhook_file.readLine()) != null){
+                jsonData.append(line);
+            }
+            System.out.println(jsonData);
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find test data file!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new JSONObject(jsonData.toString());
     }
 
 }
