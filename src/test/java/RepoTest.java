f68850fcf2ccaba4676d1c7e907331af4a01039d
worlund <worlund@kth.se>
2018-02-05 15:39:54 +0100

    feat: run gradle check

---
+++ src/test/java/RepoTest.java
@@ -0,0 +1,22 @@
+import ciserver.RepoHandler;
+
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/*
+    Test functions related to doing something on an existing repository
+ */
+public class RepoTest {
+    @Test
+    public void testRunCheck() {
+        //Contract: Test that runCheck runs and returns a respsonse string
+        File project = new File(System.getProperty("user.dir")+"//temp-git//dummy");
+        String gradleResponse = RepoHandler.runCheck(project);
+        // assert that the response string contains something
+        assertFalse(gradleResponse.isEmpty());
+    }
+}
